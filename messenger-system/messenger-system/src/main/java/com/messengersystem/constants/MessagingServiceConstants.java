c20ad61389b656a76c5b136850167b375335d3c3
Mohit Sharma <32796669+Mohitsmr23@users.noreply.github.com>
2023-12-17 22:29:26 +0530

    Added constants for send message, getChatHistory and getUnread messages api for messenger service

--- messenger-system/messenger-system/src/main/java/com/messengersystem/constants/MessagingServiceConstants.java
+++ messenger-system/messenger-system/src/main/java/com/messengersystem/constants/MessagingServiceConstants.java
@@ -10,5 +10,15 @@ public class MessagingServiceConstants {
   public static final String SUCCESS_LOGOUT_MESSAGE = "Logged out successfully";
   public static final String LOGIN_SUCCESSFUL = "Login successful";
   public static final String WRONG_PASSWORD = "Please enter correct password";
+  public static final String USERNAME_KEYWORD = "username:";
+  public static final String TEXTS_KEYWORD = "texts:";
+  public static final String COMMA_DELIMETER = ",";
+  public static final String NO_UNREAD_MESSAGES = "No new messages";
+  public static final String UNREAD_MESSAGES = "You have message(s)";
+  public static final String OPENING_CURLY_BRACE = "{";
+  public static final String CLOSING_CURLY_BRACE = "}";
+  public static final String COLON = ":";
+  public static final String LOGIN_MESSAGE = "Please login";
+  public static final String MESSAGE_SENT = "Message sent successfully";
 
 }
